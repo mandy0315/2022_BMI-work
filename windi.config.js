e243ba6612b5055f8eda5bfcd7073f4a8296ae14
Mandy Tsai <mandy3152618@gmail.com>
2022-07-31 16:07:05 +0800

    add: windiCSS 設定

--- windi.config.js
+++ windi.config.js
@@ -1,4 +1,40 @@
+import colors from "windicss/colors";
+
+const customColors = {
+  "c-background": "#f5f5f5",
+  "c-dark": "#4A4A4A",
+  "c-gray": "#424242",
+  "c-yellow": "#FFD366",
+  "c-orange-500": "#FF6C03",
+  "c-orange-300": "#FF982D",
+  "c-red-500": "#FF1200",
+  "c-green": "#86D73F",
+  "c-blue": "#31BAF9 ",
+};
+
 export default {
-  attributify: true,
-  shortcuts: {},
+  extract: {
+    // 啟用/禁用 windicss 配置
+    include: ["src/**/*.{vue,html,jsx,tsx}"],
+    exclude: ["node_modules", ".git"],
+  },
+  shortcuts: {
+    "box-container": "mx-auto max-w-768px",
+  },
+  //主題
+  theme: {
+    screens: {
+      // 響應式
+      phone: { max: "768px" },
+      "sm-phone": { max: "414px" },
+    },
+    colors: {
+      ...customColors,
+      ...colors,
+    },
+    backgroundColor: {
+      ...customColors,
+      ...colors,
+    },
+  },
 };
