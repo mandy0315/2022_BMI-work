a776364a2c2054f6066f90c7baf47fccbcc33d22
Mandy Tsai <mandy3152618@gmail.com>
2022-07-31 16:24:03 +0800

    add: 新增路徑

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/", // 開始路徑
+  base: "/firebase-practice/", // 開始路徑
   plugins: [
     vue(),
     WindiCSS({
